563e80f0917e2f1dbf03714733d52566b8dfa88b
Сергей <logut7@gmail.com>
2019-07-03 16:45:07 +0300

    Добавил tr сверху и снизу

--- doska.js
+++ doska.js
@@ -1,12 +1,12 @@
 var table = document.createElement("table");
-for (i=1; i<11; i++) {
+for (i=0; i<8; i++) {
   var tr = document.createElement("tr");
-  for (k=1; k<11; k++) {
+  for (k=0; k<10; k++) {
     var td = document.createElement("td");
     if(i%2 == k%2) {
       td.className = "white";
     } else {
-      td.className = "black";
+      td.className = "gray";
     }
     tr.appendChild(td);
   }
@@ -14,5 +14,19 @@ for (i=1; i<11; i++) {
 }
 document.body.appendChild(table);
 
-var td = document.getElementsByClassName("white");
-td.style.background="red";
+// var td = document.getElementsByClassName("white");
+// td.style.color="red";
+
+
+// Получить элементы по имени тега
+// var row1 = document.getElementsByTagName('tr')[0];
+// console.log(row1);
+var row1 = document.querySelector("table");
+var newElement = document.createElement("tr");
+table.insertBefore(newElement,table.children[0]);
+var newElement = document.createElement("tr");
+table.appendChild(newElement,table);
+newElement = document.createElement("td")
+newElement.className = "white";
+// table.appendChild(newElement,table.children);
+// row1.style.border="green";
