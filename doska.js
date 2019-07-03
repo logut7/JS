236e8bfdffacc580c58298b2dde53cb537c712b8
Сергей <logut7@gmail.com>
2019-07-03 15:39:25 +0300

    Update doska.js

--- doska.js
+++ doska.js
@@ -1,7 +1,7 @@
 var table = document.createElement("table");
-for (i=1; i<9; i++) {
+for (i=1; i<11; i++) {
   var tr = document.createElement("tr");
-  for (k=1; k<9; k++) {
+  for (k=1; k<11; k++) {
     var td = document.createElement("td");
     if(i%2 == k%2) {
       td.className = "white";
@@ -13,3 +13,6 @@ for (i=1; i<9; i++) {
   table.appendChild(tr);
 }
 document.body.appendChild(table);
+
+var td = document.getElementsByClassName("white");
+td.style.background="red";
